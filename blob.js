0116eed401659b12893cbb8584adcf38afa50e44
Arcyvilk <blazemakerrr@gmail.com>
2017-09-12 12:22:51 +0200

    Complete redesign of the bot
    Now the only thing bot needs is to be added to a server which has
    emotes, and all those emotes are instantly available for his use

--- blob.js
+++ blob.js
@@ -1,67 +1,41 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 var fs = require('fs');
-var commands = {};
+var emojis = {};
 
 const token = process.env.DISCORD_API_TOKEN;
 bot.login(token);
 
 bot.on('ready', () => {
     var data = Date.now();
-
-    fs.readFile('commands.json', 'utf8', (err, jsonContents) => {
-        if (err)
-            console.log(err);
-        commands = JSON.parse(jsonContents);
-    });
     bot.user.setPresence({ game: { name: "e!help for help", type: 0 } });
     console.log(`${data} - Blobbot reports for duty!`);
+
+    if (fetchEmojis())
+        console.log(`${data} - emoji fetching succesful!`);
 });
 
 bot.on('message', message => {
     var m = message.content;
 
     if (!message.author.bot) {
-        if (m.startsWith('e!')) {
-            if (m.startsWith('e!add')) {
-                if (m.indexOf('|') == -1) {
-                    message.channel.send(':no_entry_sign: The syntax for this command is ``e!add <command_name> | <emote>``\n' +
-                        'Example: ``e!add viktor| :viktor:``');
-                    return;
-                }
-                var emote = m.substring(5).trim().split('|');
-                if (commands.hasOwnProperty(emote[0])) {
-                    message.channel.send(`:no_entry_sign: Emote named ${emote[0]} already exists in the database.`)
-                    return;
-                }
-                commands[emote[0].trim()] = emote[1].trim();
-                fs.writeFile('commands.json', JSON.stringify(commands), err => {
-                    if (err)
-                        console.log(err);
-                    message.channel.send(`:white_check_mark: Emote ${emote[0]} added!`)
-                });
+        if (message.content.startsWith(':') && message.content.endsWith(':')) {
+            var m = message.content.substring(1, message.content.length - 1);
+            if (emojis.hasOwnProperty(m)) {
+                message.channel.send(`**${message.author.username}**: <:${m}:${emojis[m]}>`);
+                message.delete(3000);
             }
-            if (m.startsWith('e!list')) {
-                var list = '**List of emotes:** ';
-                for (i in commands)
-                    list += `| ${i} `;
-                message.channel.send(list);
-                return;
-            }
-            if (m.startsWith('e!help')) {
-                var list = '**Commands:** \n- e!list \n- e!<emote_name> \n- e!add <emote_name>|<emote>';
-                message.channel.send(list);
-                return;
-            }
-            if (!m.startsWith('e!list') && !m.startsWith('e!add')) {
-                var cmd = m.substring(2).trim();
-                if (!commands.hasOwnProperty(cmd)) {
-                    message.channel.send(':no_entry_sign: Such emote doesn\'t exist.');
-                    return;
-                }
-                message.channel.send(`${message.author.username}: ${commands[cmd]}`);
-            }
-            message.delete(3000);
         }
     }
 });
+
+function fetchEmojis() {
+    var g = bot.guilds.array();
+
+    for (i in g) {
+        var e = g[i].emojis.array();
+        for (j in e)
+            emojis[e[j].name] = e[j].id;
+    }
+    return true;
+};
